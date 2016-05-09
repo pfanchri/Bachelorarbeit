0f7ef20b28a6d62abe1cc008e7251456e6360343
pfanchri <pfanchri@web.de>
2016-05-09 12:49:57 +0200

    LED 2 von Evaluattion Board blinken

--- Dave/Basisstation/Testprojekt/main.c
+++ Dave/Basisstation/Testprojekt/main.c
@@ -20,14 +20,18 @@ int main(void)
   //if(status == DAVE_STATUS_FAILURE)
 
 
- // XMC_GPIO_SetMode(XMC_GPIO_PORT1, 0, XMC_GPIO_MODE_OUTPUT_PUSH_PULL);
-  //XMC_GPIO_SetOutputHigh(XMC_GPIO_PORT1, 0);
+  XMC_GPIO_SetMode(XMC_GPIO_PORT1, 0, XMC_GPIO_MODE_OUTPUT_PUSH_PULL);
+  XMC_GPIO_SetOutputHigh(XMC_GPIO_PORT1, 0);
 
 
   XMC_GPIO_SetMode(XMC_GPIO_PORT1, 1, XMC_GPIO_MODE_OUTPUT_PUSH_PULL);
     XMC_GPIO_SetOutputHigh(XMC_GPIO_PORT1, 1);
 
-    for(int i=0; i<500; i++ )
+while (1){
+    for(int i=0; i<500000; i++ ) XMC_GPIO_SetOutputHigh(XMC_GPIO_PORT1, 0);
+
+    for(int j=0; j<500000; j++ ) XMC_GPIO_SetOutputLow(XMC_GPIO_PORT1, 0);
+}
 
   /* Placeholder for user application code. The while loop below can be replaced with user application code. */
   while(1U)
