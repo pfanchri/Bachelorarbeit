7991c4f6e115eec6e55082dfa6949e5c654bbffd
pfanchri <christofpfannenmuller@fau-stud-6-36.rrze.uni-erlangen.de>
2016-06-22 09:33:43 +0200

    Macros für Pins und Ports des Boards erstellt

--- Dave/Basisstation/Basisstation/Main.c
+++ Dave/Basisstation/Basisstation/Main.c
@@ -7,6 +7,28 @@
 #include "Header_general.h" //including all Header files
 
 
+void set_TDA_status(uint8_t TDA, uint8_t status){
+XMC_GPIO_SetMode(XMC_GPIO_PORT1, led, XMC_GPIO_MODE_OUTPUT_PUSH_PULL);
+if(status==0){
+//set P_ON Pin low
+if(TDA==1) XMC_GPIO_SetOutputLow(XMC_GPIO_PORT1, led);
+if(TDA==1) XMC_GPIO_SetOutputLow(XMC_GPIO_PORT1, led);
+
+}else if(status==1){
+//set P_ON Pin high
+
+}
+
+}
+
+void led_on(uint8_t led){
+
+}
+
+void led_off(uint8_t led){
+
+}
+
 
 int main(void) {
 	uint16_t spi_array_tx[10] = { 0 };
