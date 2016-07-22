441c44e5a7721780b74f1db13d2d1cde40b7b2b1
pfanchri <pfanchri@web.de>
2016-07-22 10:40:37 +0200

    weiter aufgeräumt in Main

--- Dave/Basisstation/Basisstation/Main.c
+++ Dave/Basisstation/Basisstation/Main.c
@@ -119,30 +119,7 @@ int main(void) {
 //TESTMODUL-ENDE---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-	tda5340_set_mode_and_config(TDA1, RX_MODE, 0);
-
-
-
-
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "Serial Number TDA1\r\n", 20);
-	COM_send_int_as_string(tda5340_get_serial_number(TDA1));
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "Serial Number TDA2\r\n", 20);
-	COM_send_int_as_string(tda5340_get_serial_number(TDA2));
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "Serial Number TDA3\r\n", 20);
-	COM_send_int_as_string(tda5340_get_serial_number(TDA3));
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "Serial Number TDA4\r\n", 20);
-	COM_send_int_as_string(tda5340_get_serial_number(TDA4));
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "Serial Number TDA5\r\n", 20);
-	COM_send_int_as_string(tda5340_get_serial_number(TDA5));
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "Serial Number TDA6\r\n", 20);
-	COM_send_int_as_string(tda5340_get_serial_number(TDA6));
-	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
+	tda5340_set_mode_and_config(TDA1, RX_MODE, 0);// ANPASSEN AN MEJHRE TDAs
 
 
 
@@ -151,57 +128,6 @@ int main(void) {
 
 
 
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "Start Reading FIFO TDA 3\r\n", 26);
-//
-//	int8_t data_send[10] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
-//	int8_t data_rec[10];
-//	uint8_t lenght = 10;
-//	tda5340_fifo_rw(TDA3, 1, data_send, &lenght);
-//	tda5340_fifo_rw(TDA3, 0, data_rec, &lenght);
-//	COM_send_int_as_string(data_rec[0]);
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-//
-//	COM_send_int_as_string(data_rec[1]);
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-//
-//	COM_send_int_as_string(data_rec[2]);
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-//
-//	COM_send_int_as_string(data_rec[3]);
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-//
-//	COM_send_int_as_string(data_rec[4]);
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-//
-//	COM_send_int_as_string(data_rec[5]);
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-//
-//	COM_send_int_as_string(data_rec[6]);
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-//
-//	COM_send_int_as_string(data_rec[7]);
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-//
-//	COM_send_int_as_string(data_rec[8]);
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-//
-//	COM_send_int_as_string(data_rec[9]);
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-//
-//	COM_send_int_as_string(data_rec[10]);
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-//
-//	CDC_Device_SendData(&VirtualSerial_CDC_Interface, "\r\n", 2);
-
-	CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
-
-//	set_TDA_status(TDA_ALL, 0);
-//	set_TDA_status(TDA_ALL, 1);
-//
-//	set_TDA_status(TDA_ALL, 0);
-//	set_TDA_status(TDA_ALL, 1);
-//	set_TDA_status(TDA_ALL, 0);
-//	set_TDA_status(TDA_ALL, 1);
 
 //function for general test purposes
 //general_test();
